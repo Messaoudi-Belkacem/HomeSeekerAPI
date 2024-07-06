79d0ef6d245447fd8d977b388a64c4fe1e49056c
HP <wailmessaoudi806@gmail.com>
2024-07-06 11:44:24 +0100

    Add phone number field to user model class

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,6 +1,8 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
+@Getter
 @Entity
 @Table(name = "user")
 public class User implements UserDetails {
@@ -17,52 +20,34 @@ public class User implements UserDetails {
     @Column(name = "id")
     private Integer id;
 
+    @Setter
     @Column(name = "first_name")
     private String firstName;
 
+    @Setter
     @Column(name = "last_name")
     private String lastName;
 
+    @Setter
     @Column(name = "username")
     private String username;
 
+    @Setter
     @Column(name = "password")
     private String password;
 
+    @Setter
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Setter
     @Enumerated(value = EnumType.STRING)
     private Role role;
 
 
+    @Setter
     @OneToMany(mappedBy = "user")
     private List<Token> tokens;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUsername() {
-        return username;
-    }
 
     @Override
     public boolean isAccountNonExpired() {
@@ -84,36 +69,9 @@ public class User implements UserDetails {
         return true;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public List<Token> getTokens() {
-        return tokens;
-    }
-
-    public void setTokens(List<Token> tokens) {
-        this.tokens = tokens;
-    }
 }
